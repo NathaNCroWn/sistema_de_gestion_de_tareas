9f28be45c24cd61c1eff1c603ce0d51b5223c753
Nathan <jhonatandlopez@gmail.com>
2024-06-01 03:49:50 -0500

    Conexion con la base de datos que se encuentra alojada en Render e implementacion del archivo tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import server from "./server";
+import connectionDB from "./config/connection";
 
 const appi = express();
 
@@ -8,6 +9,9 @@ appi.use(express.json());
 
 //Puerto de conexion
 const PORT = 3000;
+
+//conexion base de datos.
+connectionDB();
 
 server.listen(PORT, () => {
   console.log(`Api corriendo en le puerto ${PORT}`);
